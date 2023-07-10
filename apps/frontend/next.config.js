5212b5b7b01ad2eee11fa84e35bf0d1d2cd3ee29
Mikael Pettersson <evanion@icloud.com>
2023-07-10 18:19:17 +0200

    add gluestack

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const { withGluestackUI } = require('@gluestack/ui-next-adapter');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -10,13 +11,14 @@ const nextConfig = {
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: true,
   },
 };
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  // withGluestackUI, // uncomment this line to enable Gluestack UI
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
